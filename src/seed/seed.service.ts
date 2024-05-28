667d14da4e014e4bfc36665428992a5e0ea8d96f
JorgeYael-CB <jorge.yael333@gmail.com>
2024-05-28 15:37:46 -0600

    Agregamos un custom provider y usamos el patron adaptador

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke.response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 
 
 @Injectable()
 export class SeedService {
 
-  private readonly httpAxios:AxiosInstance = axios
-
-
   constructor(
     @InjectModel( Pokemon.name )
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly httpAdapter: AxiosAdapter,
   ){}
 
 
   async executeSeed(){
-    const { data } = await this.httpAxios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650&offset=0`);
+    const data = await this.httpAdapter.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650&offset=0`);
     const pokemonToInsert: {name:string, no:number}[] = [];
 
     // Borramos los elementos previos
