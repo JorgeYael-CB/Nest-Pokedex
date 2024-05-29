a6dd3f5fcb29b4af617b3ed0013a480cc4266b01
JorgeYael-CB <jorge.yael333@gmail.com>
2024-05-29 10:18:40 -0600

    Agregamos el puerto de las variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,10 @@ async function bootstrap() {
     })
   )
 
-  await app.listen(3000);
+  const port = +process.env.PORT;
+
+  await app.listen( port );
+  console.log(`Server Running on port ${port}`);
 };
 
 
