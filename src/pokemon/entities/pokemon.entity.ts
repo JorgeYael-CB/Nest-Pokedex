c0e23377287f0a54982cdef3bdfeedafea2c73e9
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-27 17:45:49 -0600

    Agregando modelo de pokemon en DB

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,3 +1,24 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
 
 // como se ve en la base de datos
-export class Pokemon {}
+@Schema()
+export class Pokemon extends Document {
+
+  @Prop({
+    unique: true,
+    index: true, // el indice sabe donde esta el elemento que buscas.
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+
+}
+
+
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon );
